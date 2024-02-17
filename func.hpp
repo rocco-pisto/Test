00c7626e9aca2f8bff3513f6da9ba3c729f040b2
Rocco Pisto <roccopisto99@gmail.com>
2024-02-17 11:21:33 +0100

    2_Doppio

---
+++ func.hpp
@@ -0,0 +1,8 @@
+#ifndef F_HPP
+#define F_HPP
+
+bool check(int a, int b) {
+	return a == b;
+}
+
+#endif
--- main.cpp
+++ main.cpp
@@ -5,5 +5,5 @@ int main() {
 	cout << "Inserisci numero: <<endl;
 	int n;
 	cin >> n;
-	cout << "Ecco a te: " <<n << endl;
+	cout << "Ecco a te: " <<2*n << endl;
 }
